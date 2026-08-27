988a6a0731df58f4f6a3339a936223b629230405
huleixin <137732170+huleixin@users.noreply.github.com>
2026-08-27 11:01:31 +0800

    chore: remove debug/temp residual files from repo root

--- _show_ai.py
+++
@@ -1,8 +0,0 @@
-﻿with open("index.html","r",encoding="utf-8") as f:
-    lines=f.readlines()
-# click/keydown region around 14213
-for i in range(14210, 14260):
-    print(f"{i+1}|{lines[i]}", end="")
-print("---V2 START---")
-for i in range(14297, 14485):
-    print(f"{i+1}|{lines[i]}", end="")
--- _show_css.py
+++
@@ -1,4 +0,0 @@
-﻿with open("css/app.css","r",encoding="utf-8") as f:
-    lines=f.readlines()
-for i in range(980, 1080):
-    print(f"{i+1}|{lines[i]}", end="")
